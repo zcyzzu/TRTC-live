b8e845bb6f3f0a2c79a9fdc3d31a8647ec47731c
zcy <qq2764707016@gmail.com>
2021-01-19 10:52:23 +0800

    init

---
+++ dist_electron/bundled/js/chunk-2d0d83ad.af2222fa.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d83ad"],{"79d9":function(t,e,n){"use strict";n.r(e);var r=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",[n("h1",[t._v("this is setting")])])}],s={data(){return{date:"1.20"}}},u=s,c=n("2877"),i=Object(c["a"])(u,r,a,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0d83ad.af2222fa.js.map
